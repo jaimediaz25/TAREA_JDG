534dba771a2438e06be8c8e4f1a1cb3f20a1c662
jaimediaz25 <al222310433@gmail.com>
2024-09-18 21:30:54 -0600

    Añadida la función 3

--- funcion_1.java
+++ funcion_1.java
@@ -5,13 +5,15 @@ public class funcion_1 {
         System.out.println("FUNCION 1");
     }
 
-  
     public static void funcion1() {
         System.out.println("FUNCION 1");
     }
 
-   
     public static void funcion2() {
         System.out.println("FUNCION 2");
+    }
+
+    public static void funcion3() {
+        System.out.println("FUNCION 3");
     }
 }
